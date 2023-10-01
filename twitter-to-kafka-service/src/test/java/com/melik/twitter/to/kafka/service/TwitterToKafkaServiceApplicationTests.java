01ee1f0fc20d1543db11b3486a627a293395949d
mselvi <mselvi@uni-yaz.com>
2023-10-01 15:46:49 +0300

    added twitter to kafka test application and pom file

---
+++ twitter-to-kafka-service/src/test/java/com/melik/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.melik.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author mselvi
+ * @Created 23.09.2023
+ */
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
