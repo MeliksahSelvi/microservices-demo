6e1404ef62383a79e8c2cb85808b0d0d49644e1b
mselvi <mselvi@uni-yaz.com>
2023-10-01 14:58:43 +0300

    added test application to kafka to elastic microservice

---
+++ kafka-to-elastic-service/src/test/java/com/melik/kafka/to/elastic/service/KafkaToElasticServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package com.melik.kafka.to.elastic.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author mselvi
+ * @Created 30.09.2023
+ */
+
+
+@SpringBootTest
+public class KafkaToElasticServiceApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
