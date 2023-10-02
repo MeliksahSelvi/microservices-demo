31d1e1b8bfc9b71a8930cbfb1aa53baf71682323
mselvi <mselvi@uni-yaz.com>
2023-10-02 10:03:07 +0300

    added paths to ignore

--- elastic-query-service/src/main/java/com/melik/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/melik/elastic/query/service/api/ElasticDocumentController.java
@@ -4,6 +4,11 @@ import com.melik.elastic.query.service.business.ElasticQueryService;
 import com.melik.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.melik.elastic.query.service.model.ElasticQueryServiceResponseModel;
 import com.melik.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import org.slf4j.Logger;
@@ -24,10 +29,10 @@ import java.util.List;
  * produces kısmında verdiğimiz ifade request'lerin accept header kısmına koyulmalı.
  * */
 @RestController
-@RequestMapping(value = "/documents",produces = "application/vnd.api.v1+json")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
 
-    private final static Logger LOG= LoggerFactory.getLogger(ElasticDocumentController.class);
+    private final static Logger LOG = LoggerFactory.getLogger(ElasticDocumentController.class);
 
     private final ElasticQueryService elasticQueryService;
 
@@ -35,37 +40,81 @@ public class ElasticDocumentController {
         this.elasticQueryService = elasticQueryService;
     }
 
+    @Operation(summary = "Get all elastic documents")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Succesfull Response", content = {
+                    @Content(
+                            mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping("/")
-    public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments(){
-        List<ElasticQueryServiceResponseModel> response=elasticQueryService.getAllDocuments();
-        LOG.info("ElasticSearch returned {} of documents",response.size());
+    public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
+        List<ElasticQueryServiceResponseModel> response = elasticQueryService.getAllDocuments();
+        LOG.info("ElasticSearch returned {} of documents", response.size());
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Get elastic document by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Succesfull Response", content = {
+                    @Content(
+                            mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping("/{id}")
-    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id){
-        ElasticQueryServiceResponseModel response=elasticQueryService.getDocumentById(id);
-        LOG.debug("ElasticSearch returned document with id {}",id);
+    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponseModel response = elasticQueryService.getDocumentById(id);
+        LOG.debug("ElasticSearch returned document with id {}", id);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping(value = "/{id}",produces = "application/vnd.api.v2+json")
-    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id){
-        ElasticQueryServiceResponseModel response=elasticQueryService.getDocumentById(id);
-        LOG.debug("ElasticSearch returned document with id {}",id);
+    @Operation(summary = "Get elastic document by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Succesfull Response", content = {
+                    @Content(
+                            mediaType = "application/vnd.api.v2+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponseModel response = elasticQueryService.getDocumentById(id);
+        LOG.debug("ElasticSearch returned document with id {}", id);
         ElasticQueryServiceResponseModelV2 modelV2 = getModelV2(response);
         return ResponseEntity.ok(modelV2);
     }
 
-    @GetMapping("/get-document-by-text")
-    public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel requestModel){
-        List<ElasticQueryServiceResponseModel> response=elasticQueryService.getDocumentByText(requestModel.getText());
-        LOG.info("ElasticSearch returned {} of documents",response.size());
+    @Operation(summary = "Get elastic document by text")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Succesfull Response", content = {
+                    @Content(
+                            mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @PostMapping("/get-document-by-text")
+    public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel requestModel) {
+        List<ElasticQueryServiceResponseModel> response = elasticQueryService.getDocumentByText(requestModel.getText());
+        LOG.info("ElasticSearch returned {} of documents", response.size());
         return ResponseEntity.ok(response);
     }
 
-    private ElasticQueryServiceResponseModelV2 getModelV2(ElasticQueryServiceResponseModel model){
-        ElasticQueryServiceResponseModelV2 modelV2=ElasticQueryServiceResponseModelV2.builder()
+    private ElasticQueryServiceResponseModelV2 getModelV2(ElasticQueryServiceResponseModel model) {
+        ElasticQueryServiceResponseModelV2 modelV2 = ElasticQueryServiceResponseModelV2.builder()
                 .id(Long.parseLong(model.getId()))
                 .text(model.getText())
                 .text2("Text V2")
--- elastic-query-service/src/main/java/com/melik/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/melik/elastic/query/service/config/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.melik.elastic.query.service.config;
 
 import com.melik.app.config.data.config.UserConfigData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * @Author mselvi
@@ -18,13 +23,32 @@ public class WebSecurityConfig {
 
     private final UserConfigData userConfigData;
 
+    @Value("${security.paths-to-ignore}")
+    private String[] pathsToIgnore;
     public WebSecurityConfig(UserConfigData userConfigData) {
         this.userConfigData = userConfigData;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return null;
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/**").hasRole("USER")
+                        .anyExchange()
+                        .authenticated());
+        return serverHttpSecurity.build();
+    }
 
+    @Bean
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(userConfigData.getUsername())
+                .password(passwordEncoder().encode(userConfigData.getPassword()))
+                .roles(userConfigData.getRoles());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
