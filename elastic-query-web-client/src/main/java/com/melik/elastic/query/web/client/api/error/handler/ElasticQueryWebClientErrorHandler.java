82d2deb4c462326f0b1c07770ac1f61ba6fa6e0c
mselvi <mselvi@uni-yaz.com>
2023-10-02 14:00:03 +0300

    added elastic-query-web-client specific exception and exception handler

---
+++ elastic-query-web-client/src/main/java/com/melik/elastic/query/web/client/api/error/handler/ElasticQueryWebClientErrorHandler.java
@@ -0,0 +1,73 @@
+package com.melik.elastic.query.web.client.api.error.handler;
+
+import com.melik.elastic.query.web.client.model.ElasticQueryWebClientRequestModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author mselvi
+ * @Created 02.10.2023
+ */
+
+@ControllerAdvice
+public class ElasticQueryWebClientErrorHandler {
+
+    private final static Logger LOG = LoggerFactory.getLogger(ElasticQueryWebClientErrorHandler.class);
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handle(AccessDeniedException e, Model model) {
+        LOG.error("Acces Denied Exception", e);
+        model.addAttribute("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        model.addAttribute("error_description", "You are not authorized to access this resource");
+        return "error";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e, Model model) {
+        LOG.error("Illegal Argument Exception", e);
+        model.addAttribute("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        model.addAttribute("error_description", "Illegal Argument Exception");
+        return "error";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handle(RuntimeException e, Model model) {
+        LOG.error("Service Runtime Exception", e);
+        model.addAttribute("elasticQueryWebClientRequestModel", ElasticQueryWebClientRequestModel.builder().build());
+        model.addAttribute("error", "Could not get response");
+        model.addAttribute("error_description", "Service Runtime Exception");
+        return "home";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handle(Exception e, Model model) {
+        LOG.error("Internal Server Error", e);
+        model.addAttribute("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        model.addAttribute("error_description", "A server error occured");
+        return "error";
+    }
+
+    @ExceptionHandler(BindException.class)
+    public String handle(BindException e, Model model) {
+        LOG.error("Method Argument Validation Exception", e);
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error ->
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        model.addAttribute("elasticQueryWebClientRequestModel", ElasticQueryWebClientRequestModel.builder().build());
+        model.addAttribute("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        model.addAttribute("error_description", errors);
+        return "home";
+    }
+
+}
---
+++ elastic-query-web-client/src/main/java/com/melik/elastic/query/web/client/exception/ElasticQueryWebClientException.java
@@ -0,0 +1,21 @@
+package com.melik.elastic.query.web.client.exception;
+
+/**
+ * @Author mselvi
+ * @Created 02.10.2023
+ */
+
+public class ElasticQueryWebClientException extends RuntimeException {
+    public ElasticQueryWebClientException() {
+        super();
+    }
+
+    public ElasticQueryWebClientException(String message) {
+        super(message);
+    }
+
+    public ElasticQueryWebClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
