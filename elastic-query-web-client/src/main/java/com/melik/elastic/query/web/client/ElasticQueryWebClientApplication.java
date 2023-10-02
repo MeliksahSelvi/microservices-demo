48d6eea98330e0bb32050f2255eb70115870342a
mselvi <mselvi@uni-yaz.com>
2023-10-02 12:54:29 +0300

    added elastic query web client microservice module

---
+++ elastic-query-web-client/src/main/java/com/melik/elastic/query/web/client/ElasticQueryWebClientApplication.java
@@ -0,0 +1,19 @@
+package com.melik.elastic.query.web.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Author mselvi
+ * @Created 02.10.2023
+ */
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.melik")
+public class ElasticQueryWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryWebClientApplication.class);
+    }
+}
---
+++ elastic-query-web-client/src/test/java/com/melik/elastic/query/web/client/ElasticQueryWebClientApplicationTests.java
@@ -0,0 +1,18 @@
+package com.melik.elastic.query.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author mselvi
+ * @Created 02.10.2023
+ */
+
+@SpringBootTest
+public class ElasticQueryWebClientApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
