e1bf22c24db4af2dd74c4fd3886f071133b71fa8
mselvi <mselvi@uni-yaz.com>
2023-10-02 14:01:34 +0300

    added elastic-query-web-client spring security config

---
+++ elastic-query-web-client/src/main/java/com/melik/elastic/query/web/client/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.melik.elastic.query.web.client.config;
+
+import com.melik.app.config.data.config.UserConfigData;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @Author mselvi
+ * @Created 02.10.2023
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final UserConfigData userConfigData;
+
+    public WebSecurityConfig(UserConfigData userConfigData) {
+        this.userConfigData = userConfigData;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/**").hasRole("USER")
+                        .anyExchange()
+                        .authenticated());
+        return serverHttpSecurity.build();
+    }
+
+    @Bean
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(userConfigData.getUsername())
+                .password(passwordEncoder().encode(userConfigData.getPassword()))
+                .roles(userConfigData.getRoles());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
