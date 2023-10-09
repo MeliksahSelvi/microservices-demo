55e6e2bdfc8e6b4de3c1d5c50ab0bf5274c4676e
mselvi <mselvi@uni-yaz.com>
2023-10-09 16:15:33 +0300

    added discovery client annotation to elastic query service module

--- elastic-query-service/src/main/java/com/melik/elastic/query/service/ElasticQueryServiceApplication.java
+++ elastic-query-service/src/main/java/com/melik/elastic/query/service/ElasticQueryServiceApplication.java
@@ -2,6 +2,7 @@ package com.melik.elastic.query.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.melik")
 public class ElasticQueryServiceApplication {
 
