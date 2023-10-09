1dd80a3e5f90d455a6d742f4b4f0379a842fd5c2
mselvi <mselvi@uni-yaz.com>
2023-10-09 16:05:40 +0300

    update elasticsearch config because old config is deprecated

--- elastic/elastic-config/src/main/java/com/melik/elastic/config/ElasticsearchConfig.java
+++ elastic/elastic-config/src/main/java/com/melik/elastic/config/ElasticsearchConfig.java
@@ -1,19 +1,10 @@
 package com.melik.elastic.config;
 
 import com.melik.app.config.data.config.ElasticConfigData;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.erhlc.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Objects;
 
 /**
  * @Author mselvi
@@ -22,10 +13,10 @@ import java.util.Objects;
 
 @Configuration
 /*
-* hem elastic index client hem elastic query client moduüllerindeki repository'leri taramak için basePackages ifadesini daha genel bir hale çektik.
-* */
+ * hem elastic index client hem elastic query client moduüllerindeki repository'leri taramak için basePackages ifadesini daha genel bir hale çektik.
+ * */
 @EnableElasticsearchRepositories(basePackages = "com.melik.elastic")
-public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
     private final ElasticConfigData elasticConfigData;
 
@@ -33,26 +24,12 @@ public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
         this.elasticConfigData = elasticConfigData;
     }
 
-    @Bean
     @Override
-    public RestHighLevelClient elasticsearchClient() {
-        UriComponents serverUri= UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
-
-        return new RestHighLevelClient(
-                RestClient.builder(new HttpHost(
-                        Objects.requireNonNull(serverUri.getHost()),
-                        serverUri.getPort(),
-                        serverUri.getScheme()
-                )).setRequestConfigCallback(
-                        builder -> builder
-                                .setConnectTimeout(elasticConfigData.getConnectionTimeoutMs())
-                                .setSocketTimeout(elasticConfigData.getSocketTimeoutMs())
-                )
-        );
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchOperations(){
-        return new ElasticsearchRestTemplate((elasticsearchClient()));
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticConfigData.getConnectionUrl())
+                .withConnectTimeout(elasticConfigData.getConnectionTimeoutMs())
+                .withSocketTimeout(elasticConfigData.getSocketTimeoutMs())
+                .build();
     }
 }
